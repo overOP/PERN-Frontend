f7d81016b2a3ae118b240ef8d38a6d98846814c0
overOP <cgpradeepop@gmail.com>
2025-04-24 12:55:36 +0545

    fix : footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <>
     <footer className="bg-gray-900 text-white p-4 text-center">
-      <p>&copy; 2025 PERN by Pradeep Chaudhary</p>
+      <p>&copy; {new Date().getFullYear() // Get the current year
+      } PERN by Pradeep Chaudhary</p>
     </footer>
     </>
   )
